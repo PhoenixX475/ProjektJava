d010994ad666ac3a8a3dcf780a41de26bc03e0f9
PhoeniX475 <284615@student.pwr.edu.pl>
2025-06-08 18:56:26 +0200

    Commit 7 Dodano testy jednostkowe

---
+++ src/test/java/logic/mrowki/RobotnicaTest.java
@@ -0,0 +1,21 @@
+package logic.mrowki;
+
+import graphics.MapaPanel;
+import logic.obiekty.Lisc;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RobotnicaTest {
+    void testgrab() {
+        MapaPanel mapa = new MapaPanel(1,1,1,1);
+        Mrowisko mrowisko = new Mrowisko(1,1,mapa);
+        Robotnica r = new Robotnica(10,10,mrowisko,mapa, Color.BLACK);
+        Lisc l = new Lisc(10,10);
+        r.targeting = l;
+        r.grab();
+        assertNotNull(r.holding);
+        assertTrue(r.holding instanceof Lisc);
+    }
+}
