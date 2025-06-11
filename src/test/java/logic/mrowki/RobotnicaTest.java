82cdee46064894ccdd3462fe84867e0af3739700
PhoeniX475 <284615@student.pwr.edu.pl>
2025-06-11 22:21:49 +0200

    Commit test

--- src/test/java/logic/mrowki/RobotnicaTest.java
+++ src/test/java/logic/mrowki/RobotnicaTest.java
@@ -2,12 +2,16 @@ package logic.mrowki;
 
 import graphics.MapaPanel;
 import logic.obiekty.Lisc;
+import logic.rozne.ObiektMapy;
+import org.junit.jupiter.api.Test;
 
 import java.awt.*;
+import java.util.LinkedList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RobotnicaTest {
+    @Test
     void testgrab() {
         MapaPanel mapa = new MapaPanel(1,1,1,1,1,true);
         Mrowisko mrowisko = new Mrowisko(1,1,mapa);
@@ -17,5 +21,23 @@ public class RobotnicaTest {
         r.grab();
         assertNotNull(r.holding);
         assertTrue(r.holding instanceof Lisc);
+    }
+
+    @Test
+    void testChecking() {
+        MapaPanel mapa = new MapaPanel(1,1,1,1,1,true);
+        Mrowisko mrowisko = new Mrowisko(1,1,mapa);
+        Robotnica r = new Robotnica(10,10,mrowisko,mapa, Color.BLACK);
+        Mrowisko mrowisko1 = new Mrowisko(40,40,mapa);
+        Robotnica r1 = new Robotnica(12,12,mrowisko1,mapa, Color.BLACK);
+        LinkedList<ObiektMapy> listaobiektow = new LinkedList<>();
+        listaobiektow.add(mrowisko);
+        listaobiektow.add(mrowisko1);
+        listaobiektow.add(r);
+        listaobiektow.add(r1);
+
+        ObiektMapy obj = r.checkArea(listaobiektow);
+        assertEquals(obj,r1);
+
     }
 }
